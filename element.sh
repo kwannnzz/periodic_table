ad9ed46f71106f257717cd4d02a8d0db8802bda0
kwân kấn <junie.deeptry@gmail.com>
2024-07-08 09:44:04 +0000

    feat: done

--- element.sh
+++ element.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --csv -c"
+#select * from elements inner join properties using(atomic_number) inner join types using(type_id)
 if [[ ! $1 ]]
 then
-  echo Please provide an element as an argument.
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+  else
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1' OR name='$1'")
+  fi
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo $($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER") | while IFS="," read TYPE_ID NUMBER SYMBOL NAME MASS MELT BOIL TYPE
+    do
+      echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+    done
+  fi
 fi
